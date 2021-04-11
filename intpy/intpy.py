c2bffa3ede1289291aab97fba6013b93984531f1
JoaoLopez <JoaoLopez@joaolopez@id.uff.br>
2021-04-11 18:37:46 -0300

    Implementation of threads (with threading library) when functions and methods marked with @deterministic are called. The code still have DEBUG messages

--- intpy/intpy.py
+++ intpy/intpy.py
@@ -10,6 +10,8 @@ from . import CONSTANTES
 from .banco import Banco
 import os
 def _get_cache(func, args, functionReturnList):
+    print("CONSULTANDO O BANCO {0}({1})...".format(func.__name__, *args))
+
     #Opening connection with database for current running thread
     CONSTANTES.CONEXAO_BANCO = Banco(os.path.join(".intpy", "intpy.db"))
 
@@ -20,6 +22,7 @@ def _get_cache(func, args, functionReturnList):
         debug("cache hit for {0}({1})".format(func.__name__, args))
         functionReturnList.append(c)
 
+    print("CONSULTA AO BANCO {0}({1}) CONCLUÍDA!".format(func.__name__, *args))
 
 def _cache_exists(cache):
     return cache is not None
@@ -34,6 +37,8 @@ def _cache_data(func, fun_args, fun_return, elapsed_time):
 
 
 def _execute_func(f, functionReturnList, method_args, method_kwargs, self=None,):
+    print("EXECUTANDO A FUNÇÃO {0}({1})...".format(f.__name__, *method_args))
+    
     start = time.perf_counter()
     result_value = f(self, *method_args, **method_kwargs) if self is not None else f(*method_args, **method_kwargs)
     end = time.perf_counter()
@@ -42,9 +47,13 @@ def _execute_func(f, functionReturnList, method_args, method_kwargs, self=None,)
 
     debug("{0} took {1} to run".format(f.__name__, elapsed_time))
 
+    print("EXECUÇÃO FUNÇÃO {0}({1}) CONCLUÍDA!".format(f.__name__, *method_args))
+
     functionReturnList.append((result_value, elapsed_time))
 
 
+"""
+CÓDIGO ANTIGO
 def _method_call(f):
     @wraps(f)
     def wrapper(self, *method_args, **method_kwargs):
@@ -61,8 +70,7 @@ def _method_call(f):
 
     return wrapper
 
-"""
-CÓDIGO ANTIGO
+
 def _function_call(f):
     @wraps(f)
     def wrapper(*method_args, **method_kwargs):
@@ -79,6 +87,36 @@ def _function_call(f):
 
     return wrapper
 """
+
+
+def _method_call(f):
+    @wraps(f)
+    def wrapper(self, *method_args, **method_kwargs):
+        debug("calling {0}".format(f.__name__))
+        
+        functionReturnList = []
+        cacheSearchThread = threading.Thread(target=_get_cache, args=tuple([f, method_args, functionReturnList]))
+        cacheSearchThread.start()
+        methodExecutionThread = threading.Thread(target=_execute_func, args=tuple([f, functionReturnList, method_args, method_kwargs, self]))
+        methodExecutionThread.start()
+
+        print("inspect.getsource(f):", inspect.getsource(f))
+
+        while True:
+            if(len(functionReturnList) > 0):
+                break
+        
+        print("functionReturnList:", functionReturnList)
+
+        if(isinstance(functionReturnList[0], tuple)):
+            _cache_data(f, method_args, functionReturnList[0][0], inspect.getsource(f))
+            return functionReturnList[0][0]
+        
+        return functionReturnList[0]
+
+    return wrapper
+
+
 def _function_call(f):
     @wraps(f)
     def wrapper(*method_args, **method_kwargs):
@@ -89,6 +127,8 @@ def _function_call(f):
         cacheSearchThread.start()
         functionExecutionThread = threading.Thread(target=_execute_func, args=tuple([f, functionReturnList, method_args, method_kwargs]))
         functionExecutionThread.start()
+
+        #print("inspect.getsource(f):", inspect.getsource(f))
 
         while True:
             if(len(functionReturnList) > 0):

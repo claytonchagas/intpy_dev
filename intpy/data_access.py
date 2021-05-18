d8738562518528557ae77496cc5e5fc0dea0f35d
JoaoLopez <JoaoLopez@joaolopez@id.uff.br>
2021-05-18 19:13:56 -0300

    2nd implementation of dictionary (uses 1 dictionary): at the begining of the execution all the data cached is loaded to the dictonary before the user script starts to run.

--- intpy/data_access.py
+++ intpy/data_access.py
@@ -3,10 +3,8 @@ import re
 import hashlib
 
 from .logger.log import debug, error, warn
-
-from . import CONEXAO_BANCO
-
-DICIO_NOVOS_DADOS = {}
+from .banco import Banco
+import os
 
 def _save(file_name):
     CONEXAO_BANCO.executarComandoSQLSemRetorno("INSERT OR IGNORE INTO CACHE(cache_file) VALUES ('{0}')".format(file_name))
@@ -31,27 +29,11 @@ def _get_id(fun_name, fun_args, fun_source):
 def get_cache_data(fun_name, fun_args, fun_source):
     id = _get_id(fun_name, fun_args, fun_source)
     
-    #Verificando se há dados salvos em "DICIO_NOVOS_DADOS"
-    if(id in DICIO_NOVOS_DADOS):
-        return DICIO_NOVOS_DADOS[id]
-    
-    #Verificando se há dados salvos no banco
-    list_file_name = _get(_get_file_name(id))
-    file_name = None
-    if(len(list_file_name) == 1):
-        file_name = list_file_name[0]
+    #Checking if the result is saved in the cache
+    if(id in DATA_DICTIONARY):
+        return DATA_DICTIONARY[id]
 
-    def deserialize(id):
-        try:
-            with open(".intpy/cache/{0}".format(_get_file_name(id)), 'rb') as file:
-                return pickle.load(file)
-        except FileNotFoundError as e:
-            warn("corrupt environment. Cache reference exists for {0} in database but there is no file for it in cache folder.\
- Have you deleted cache folder?".format(fun_name))
-            autofix(id)
-            return None
-
-    return deserialize(id) if file_name is not None else None
+    return None
 
 
 def autofix(id):
@@ -63,19 +45,45 @@ def autofix(id):
 
 def create_entry(fun_name, fun_args, fun_return, fun_source):
     id = _get_id(fun_name, fun_args, fun_source)
-    DICIO_NOVOS_DADOS[id] = fun_return
+    DATA_DICTIONARY[id] = fun_return
 
 def salvarNovosDadosBanco():
     def serialize(return_value, file_name):
         with open(".intpy/cache/{0}".format(_get_file_name(file_name)), 'wb') as file:
             return pickle.dump(return_value, file, protocol=pickle.HIGHEST_PROTOCOL)
     
-    for id in DICIO_NOVOS_DADOS:
+    for id in DATA_DICTIONARY:
         debug("serializing return value from {0}".format(id))
-        serialize(DICIO_NOVOS_DADOS[id], id)
+        serialize(DATA_DICTIONARY[id], id)
 
         debug("inserting reference in database")
         _save(_get_file_name(id))
 
     CONEXAO_BANCO.salvarAlteracoes()
     CONEXAO_BANCO.fecharConexao()
+
+def deserialize(id):
+    try:
+        with open(".intpy/cache/{0}".format(_get_file_name(id)), 'rb') as file:
+            return pickle.load(file)
+    except FileNotFoundError as e:
+        warn("corrupt environment. Cache reference exists for a function in database but there is no file for it in cache folder.\
+Have you deleted cache folder?")
+        autofix(id)
+        return None
+
+
+#Opening database connection and creating select query to the database
+#to populate DATA_DICTIONARY
+CONEXAO_BANCO = Banco(os.path.join(".intpy", "intpy.db"))
+
+list_of_ipcache_files = CONEXAO_BANCO.executarComandoSQLSelect("SELECT cache_file FROM CACHE")
+DATA_DICTIONARY = {}
+for ipcache_file in list_of_ipcache_files:
+    ipcache_file = ipcache_file[0].replace(".ipcache", "")
+    
+    result = deserialize(ipcache_file)
+    if(result is None):
+        continue
+    else:
+        DATA_DICTIONARY[ipcache_file] = result

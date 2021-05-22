6cf5b65991a53f83abf21fd6b05b7b6f22228ed7
Clayton Escouper das Chagas <claytonchagas1@gmail.com>
2021-05-22 05:19:46 -0300

    Deleted output folder; refactored and fixed test's script, received arguments in Python command line, renamed from test.py to test_fib_rec.py; debug mode implemented and fixed (e.g.: DEBUG=True python3 test_fib_rec.py 20), note: default is DEBUG=False, i.e.: debug mode disabled.

---
+++ test_fib_rec.py
@@ -0,0 +1,27 @@
+#test_fib_rec.py
+
+import time
+import sys
+
+from intpy.intpy import deterministic
+
+
+@deterministic
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+#def main(n):
+def main():
+    start = time.perf_counter()
+    #print(fib(n))
+    print(fib(20))
+    print(time.perf_counter()-start)
+
+
+if __name__ == "__main__":
+    #n = float(sys.argv[1])
+    #main(n)
+    main()

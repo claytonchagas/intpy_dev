6f09030e258f8dec71a819a507da17b5b5c7a60a
Clayton Escouper das Chagas <claytonchagas1@gmail.com>
2021-05-22 05:42:57 -0300

    Deleted output folder; refactored and fixed test's script, received arguments in Python command line, renamed from test.py to test_fib_rec.py; debug mode implemented and fixed (e.g.: DEBUG=True python3 test_fib_rec.py 20), note: default is DEBUG=False, i.e.: debug mode disabled.

--- intpy/logger/log.py
+++ intpy/logger/log.py
@@ -1,12 +1,21 @@
+#log.py
+
 import datetime
+import os
+
+
+try:
+    debug_enabled = os.environ['DEBUG']
+except:
+    debug_enabled = 'False'
 
 
 def _log(mode, message):
     print("[{2}][{0}]: {1}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), message, mode))
 
-
 def debug(message):
-    _log("DEBUG", message)
+    if debug_enabled == 'True':
+        _log("DEBUG", message)
 
 
 def error(message):
@@ -15,3 +24,4 @@ def error(message):
 
 def warn(message):
     _log("WARN ", message)
+

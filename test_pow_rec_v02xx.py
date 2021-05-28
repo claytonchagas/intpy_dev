a9969aef2d38e438be7f6886ebf00923607a5f1e
Clayton Escouper das Chagas <claytonchagas1@gmail.com>
2021-05-28 02:51:00 -0300

    Adding more tests scripts to main

--- test_pow_rec_v02xx.py
+++ test_pow_rec_v02xx.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     n, m = float(sys.argv[1], float(sys.argv[2])
     start = time.perf_counter()
     print(pow(n, m))
-    salvarcache()
+    salvarCache()
     print(time.perf_counter()-start)

2049e745d8cca71d8c7e328bfbb65ca3b60141c1
Clayton Escouper das Chagas <claytonchagas1@gmail.com>
2021-06-10 16:18:55 -0300

    Refactoring to join v0.2.2.x with v0.2.x

--- intpy/__init__.py
+++ intpy/__init__.py
@@ -5,4 +5,4 @@ init_env()
 #This import is necessary because when it is executed, a select query to
 #the database is started an all the data is brought and included in the
 #DATA_DICTIONARY defined at that file
-from .data_access_v022x_1d-ad import *
+from .data_access import *

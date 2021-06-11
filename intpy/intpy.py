1d04de6f43bf83d5fc6d14bc540ad030b8f359dd
Clayton Escouper das Chagas <claytonchagas1@gmail.com>
2021-06-11 04:09:16 -0300

    Refactoring to enable command line argument parsing mode and help

--- intpy/intpy.py
+++ intpy/intpy.py
@@ -3,14 +3,60 @@ from functools import wraps
 import time
 import sys
 import importlib
+import argparse
 
 
-version = str(sys.argv[-2])
+def usage_msg():
+    return "\nIntPy's Python command line arguments help:\n\n\
+To run your experiment with IntPy use:\n\
+$ python "+str(sys.argv[0])+" program_arguments [-h] [-v version, --version version] [--no-cache]\n\n\
+To run in the IntPy DEBUG mode use:\n\
+$ DEBUG=True python "+str(sys.argv[0])+" program_arguments [-h] [-v version, --version version] [--no-cache]"
+    
 
-if version == "-v=1d-ow":
+versions = ['1d-ow', 'v021x', '1d-ad', 'v022x', '2d-ad', 'v023x', '2d-ad-t', 'v024x', '2d-ad-f', 'v025x', '2d-ad-ft', 'v026x', '2d-lz', 'v027x']
+
+intpy_arg_parser = argparse.ArgumentParser(usage=usage_msg())
+
+intpy_arg_parser.add_argument('args',
+                               metavar='program arguments',
+                               nargs='*',
+                               type=str, 
+                               help='program arguments')
+
+intpy_arg_parser.add_argument('-v',
+                              '--version',
+                               choices=versions,
+                               metavar='',
+                               nargs=1,
+                               type=str, 
+                               help='IntPy\'s mechanism version: choose one of the following options: '+', '.join(versions))
+
+intpy_arg_parser.add_argument('--no-cache',
+                              default=False,
+                              action="store_true",
+                              help='IntPy\'s disable cache')
+
+args = intpy_arg_parser.parse_args()
+
+print(args.version)
+
+if args.version == ['1d-ow'] or args.version == ['v021x']:
     v_data_access = ".data_access_v021x_1d-ow"
+elif args.version == ['1d-ad'] or args.version == ['v022x']:
+    v_data_access = ".data_access_v022x_1d-ad"
+elif args.version == ['2d-ad'] or args.version == ['v023x']:
+    v_data_access = ".data_access_v023x_2d-ad"
+elif args.version == ['2d-ad-t'] or args.version == ['v024x']:
+    v_data_access = ".data_access_v024x_2d-ad-t"
+elif args.version == ['2d-ad-f'] or args.version == ['v025x']:
+    v_data_access = ".data_access_v025x_2d-ad-f"
+elif args.version == ['2d-ad-ft'] or args.version == ['v026x']:
+    v_data_access = ".data_access_v026x_2d-ad-ft"
+elif args.version == ['2d-lz'] or args.version == ['v027x']:
+    v_data_access = ".data_access_v027x_2d-lz"
 else:
-    v_data_access = ".data_access"
+    v_data_access = ".data_access_v021x_1d-ow"
 
 
 v_data_access_import = importlib.import_module(v_data_access, package="intpy")
@@ -98,8 +144,9 @@ def _is_method(f):
     return bool(args and args[0] == 'self')
 
 
-PCACHE = str(sys.argv[-1])
-if PCACHE == "--no-cache":
+#PCACHE = str(sys.argv[-1])
+#if PCACHE == "--no-cache":
+if args.no_cache:
     def deterministic(f):
         return f
 else:
@@ -111,17 +158,18 @@ def salvarCache():
     f_salvarNovosDadosBanco()
     # salvarNovosDadosBanco()
 
+
 #On the decorator "initialize_intpy", "user_script_path" is declared
 #to maintain compatibility between different versions of IntPy
 def initialize_intpy(user_script_path):
     def decorator(f):
         def execution(*method_args, **method_kwargs):
+            #if len(sys.argv) < len(method_args)+2:
+                #print("Choose one IntPy's mechanism version")
+                #sys.exit()
             f(*method_args, **method_kwargs)
-            #print(PCACHE)
-            if PCACHE != "--no-cache":
-                #print(PCACHE)
-                salvarCache()
             #if PCACHE != "--no-cache":
-            salvarCache()
+            if args.no_cache == False:
+                salvarCache()
         return execution
     return decorator

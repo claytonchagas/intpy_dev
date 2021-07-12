0fcfc899b1fe82ed094f282d3b35e5a6fa29a427
Clayton Escouper das Chagas <claytonchagas1@gmail.com>
2021-07-12 11:43:02 -0300

    Adding Digital_RADs experiment to v0.5.x branch

---
+++ Digital_RADs/stats_colab_digi_rads.py
@@ -0,0 +1,38 @@
+import statistics
+import sys
+
+#data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def main():
+    o = sys.argv[1]
+    f = open(o, "r")
+    data = []
+    for x in f.readlines()[89::90]:
+    #for x in f.read().splitlines()[1::2]:
+    #for i in range(0, len(f1)):
+        data.append(float(x))
+    f.close()
+    print(data)
+    print("Max:", max(data))
+    print("Min:", min(data))
+    print("Mean:", statistics.mean(data))
+    print("Median:", statistics.median(data))
+    print("Standard deviation:", statistics.stdev(data))
+    print("Variance:", statistics.variance(data))
+
+    
+
+    # for i in range(0, len(data)):
+    #     if(i % 2 != 0):
+    #         print(data[i])
+    
+    # for i in range(1, len(data), 2):
+    #     print(data[i])
+
+    # for i in data[::2]:
+    #     print(data[i])
+
+
+if __name__=="__main__":
+    main()
